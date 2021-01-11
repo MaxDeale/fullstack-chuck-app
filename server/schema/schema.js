e51273ae614d727cb7f752ece01870a4df625d32
maxdeale <maxericdeale@gmail.com>
2021-01-11 07:08:45 -0800

    initial

---
+++ server/schema/schema.js
@@ -0,0 +1,63 @@
+import graphql from "graphql";
+import axios from "axios";
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+} = graphql;
+
+const JokeCategoryType = new GraphQLObjectType({
+  name: "JokeCategory",
+  fields: () => ({
+    category: { type: GraphQLString },
+  }),
+});
+
+const RandomJokeType = new GraphQLObjectType({
+  name: "RandomJoke",
+  fields: () => ({
+    id: { type: GraphQLString },
+    value: { type: GraphQLString },
+    category: { type: JokeCategoryType },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    categories: {
+      type: new GraphQLList(JokeCategoryType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.chucknorris.io/jokes/categories")
+          .then((res) => res.data);
+      },
+    },
+
+    randomJokeByCategory: {
+      type: RandomJokeType,
+      args: {
+        category: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        const category = "food";
+        return axios
+          .get(
+            `https://api.chucknorris.io/jokes/random?category=${args.category}`
+          )
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+const schema = new GraphQLSchema({
+  query: RootQuery,
+});
+
+export default schema;
