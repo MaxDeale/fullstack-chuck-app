a6ff80024102a1906f7a5e5dc5ed58a6bef76752
maxdeale <maxericdeale@gmail.com>
2021-01-12 08:29:05 -0800

    adding style

--- client/src/screens/JokeScreen.tsx
+++ client/src/screens/JokeScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import approved from "../approved.jpg";
 import { Link } from "react-router-dom";
+import jokeScreenStyle from "./jokeScreenStyle.module.css";
 
 interface Props {
   joke: {
@@ -11,7 +12,7 @@ interface Props {
 
 const JokeScreen: React.FC<Props> = ({ joke: { id, value } }) => {
   return (
-    <div>
+    <div className={jokeScreenStyle.jokescreenMain}>
       <img src={approved} alt="" />
       <h2>{value}</h2>
       <div>
