0b8209bd641f008f29082236322ed08efeed84f2
maxdeale <maxericdeale@gmail.com>
2021-01-14 16:17:22 +0200

    dep

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import schema from "./schema/schema.js";
+import path from "path";
 import dotenv from "dotenv";
 
 //simple express server using graphql
+const __dirname = path.resolve();
 dotenv.config();
 const app = express();
 
@@ -16,10 +18,10 @@ app.use(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
