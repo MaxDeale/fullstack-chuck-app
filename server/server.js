5fe41b18836f007091a647c59db328f4af202a42
maxdeale <maxericdeale@gmail.com>
2021-01-14 16:01:27 +0200

    fixed bug

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
